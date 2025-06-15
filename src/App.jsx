5352cf647feeeda4b6fecd7e88e7a07f52565095
Sudhanshu <vanshdhawan03@gmail.com>
2025-06-15 11:40:02 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
-      style={{ backgroundImage: "url('/src/assets/todo.webp')" }}>
+      style={{ backgroundImage: "url('/public/assets/todo.webp')" }}>
 
       <div className="bg-white/20 backdrop-blur-lg shadow-2xl rounded-3xl p-8 w-full max-w-lg border border-white/20">
         <h1 className="text-3xl font-bold text-white text-center mb-6 drop-shadow-lg">
